b8dfdfedcd33066b8c115f44aa57564de92a03a3
PJoserAtauchi <pjoseratauchi@MacBook-Pro.local>
2020-04-23 03:35:32 -0500

    v 1.0.5

--- R/zx.R
+++ R/zx.R
@@ -1,3 +1,5 @@
-tv <- utils::packageVersion("sdStaf")
-m <- paste0("This is version ", tv, " of the \"sdStaf\" package, for testing only\n")
-packageStartupMessage(m)
+.onAttach <- function(libname, pkgname) {
+  tv <- utils::packageVersion("sdStaf")
+  m <- paste0("This is version ", tv, " of the \"sdStaf\" package, for testing only\n")
+  packageStartupMessage(m)
+}
